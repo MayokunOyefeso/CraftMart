ef0e78e0053125cd95f367843116eef5b531f5bc
Mayokun Oyefeso <mayokunoyefeso@gmail.com>
2024-01-16 20:11:49 -0600

    Initial Commit

---
+++ CraftMart/CraftMartApp.swift
@@ -0,0 +1,17 @@
+//
+//  CraftMartApp.swift
+//  CraftMart
+//
+//  Created by Oluwamayokun Oyefeso on 1/16/24.
+//
+
+import SwiftUI
+
+@main
+struct CraftMartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
